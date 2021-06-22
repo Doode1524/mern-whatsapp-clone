93dee66eea5a3ca4ad1a1f3404528e4534cc1287
joey <doode1524@gmail.com>
2021-06-22 17:43:56 -0400

    style app.css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import './App.css';
-import Chat from './Chat';
-import Sidebar from './Sidebar';
+import "./App.css";
+import Chat from "./Chat";
+import Sidebar from "./Sidebar";
 
 function App() {
   return (
     <div className="app">
-      <Sidebar />
-      <Chat />
+      <div className="app__body">
+        <Sidebar />
+        <Chat />
+      </div>
     </div>
   );
 }

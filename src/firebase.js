c5be4daa4dfad2ca3877bdc52c794f8c9d3e4d62
joey <doode1524@gmail.com>
2021-06-22 17:20:42 -0400

    firebase config

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyA1BTQGxUr5YbNyAp_ohFRs45Q74uyzgkY",
+  authDomain: "mern-whatsapp-clone-e5f90.firebaseapp.com",
+  projectId: "mern-whatsapp-clone-e5f90",
+  storageBucket: "mern-whatsapp-clone-e5f90.appspot.com",
+  messagingSenderId: "465811240867",
+  appId: "1:465811240867:web:e2ea3d215a4b035ca04c16",
+};
